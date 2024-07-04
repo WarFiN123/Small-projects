b5430a157efa440a438a9684a693b31d32de1610
WarFiN <rajvirgoenka@gmail.com>
2024-07-04 22:47:22 +0530

    Divide the Calculator project into multple files.

---
+++ C++/Calculator/functions.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+
+long double hypotenuse_cal(long double, long double);
+long double leg_cal(long double, long double);
+long double calculate(long double, long double, long double, std::string);
